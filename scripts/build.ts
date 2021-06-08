e3bab4aeb7629e396f3894222ac30223011c69a5
ShiratoriTenta <tenta1111swan@gmail.com>
2021-06-08 16:22:20 +0900

    add build scripts

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,1 +1,66 @@
-console.log(1);
+import yargs from "yargs";
+const childProcess = require("child_process");
+const path = require("path");
+const { promisify } = require("util");
+
+const exec = promisify(childProcess.exec);
+
+async function run(params: any) {
+  console.log(path.resolve("./src"));
+  const babelConfigPath = path.resolve(__dirname, "../babel.config.json");
+  const srcDir = path.resolve("./lib");
+  const outDir = path.resolve("./dist");
+  const extensions = [".js", ".ts", ".tsx"];
+
+  const babelArgs = [
+    "--config-file",
+    babelConfigPath,
+    "--extensions",
+    `"${extensions.join(",")}"`,
+    srcDir,
+    "--out-dir",
+    outDir,
+  ];
+
+  const babel = ["yarn babel", ...babelArgs].join(" ");
+
+  const tscArgs = ["--emitDeclarationOnly"];
+  const tsc = ["yarn tsc", ...tscArgs].join(" ");
+
+  const babelResult = await exec(babel, {});
+
+  if (babelResult.stderr) {
+    throw new Error(`'${babel}' failed with \n${babelResult.stderr}`);
+  }
+
+  const tscResult = await exec(tsc, {});
+
+  if (tscResult.stderr) {
+    throw new Error(`'${babel}' failed with \n${tscResult.stderr}`);
+  }
+}
+
+yargs
+  .command({
+    command: "$0",
+    builder: (command) => {
+      return command
+        .option("largeFiles", {
+          type: "boolean",
+          default: false,
+          describe:
+            "Set to `true` if you know you are transpiling large files.",
+        })
+        .option("onlyESModules", {
+          type: "boolean",
+          default: false,
+          describe: "Set to `true` if the package only ships with ES modules.",
+        })
+        .option("out-dir", { default: "./build", type: "string" })
+        .option("verbose", { type: "boolean" });
+    },
+    handler: run,
+  })
+  .parse();
+
+console.log("a");
