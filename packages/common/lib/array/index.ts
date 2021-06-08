42b62668598e72cca54e94a52cf89a970fb30272
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-08 19:19:15 +0900

    range

--- packages/common/lib/array/index.ts
+++ packages/common/lib/array/index.ts
@@ -1,3 +1,50 @@
-export function* arrayWithKey(length: number) {
-  return [...Array(length).keys()];
+/**
+ * 0～lengthを値を持つ配列を返す
+ * @param  {number} length
+ * @returns number[]
+ */
+function range(length: number): number[];
+/**
+ * start～end-1を値を持つ配列を返す
+ * @param  {number} start
+ * @param  {number} end
+ * @returns number[]
+ */
+function range(start: number, end: number): number[];
+/**
+ * start, start + step, start + step * 2, ... start + step * floor((end - start) / step) を値を持つ配列を返す
+ * @param  {number} start
+ * @param  {number} end
+ * @param  {number} step
+ * @returns number[]
+ */
+function range(start: number, end: number, step: number): number[];
+function range(startOrLength: number, end?: number, step?: number): number[] {
+  let { length, start } = init(startOrLength, end, step);
+  function* gen(length: number, cb: (k: number) => number) {
+    for (const k of Array(length).keys()) {
+      yield cb(k);
+    }
+  }
+  return [...gen(length, (i) => i * (step ?? 1) + start)];
+}
+export { range };
+function init(
+  startOrLength: number,
+  end: number | undefined,
+  step: number | undefined
+) {
+  if (step != null) {
+    return {
+      length: Math.ceil((end! - startOrLength) / step),
+      start: startOrLength,
+    };
+  } else if (end != null) {
+    return {
+      length: end - startOrLength,
+      start: startOrLength,
+    };
+  } else {
+    return { length: startOrLength, start: 0 };
+  }
 }
