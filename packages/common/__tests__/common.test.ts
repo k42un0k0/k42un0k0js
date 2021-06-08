7c9ecfb8436dc7f19beeab7adaf6bf5e3e5ee496
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-08 19:19:15 +0900

    range

--- packages/common/__tests__/common.test.ts
+++ packages/common/__tests__/common.test.ts
@@ -1,5 +1,19 @@
-import { common } from "../lib";
+import { range } from "../lib/array";
 
-describe("common", () => {
-  it.todo("needs tests");
+describe("range", () => {
+  it("return array when given a arg", () => {
+    const actual = range(5);
+    const expectVal = [0, 1, 2, 3, 4];
+    expect(actual).toEqual(expectVal);
+  });
+  it("return array when given 2 args", () => {
+    const actual = range(4, 7);
+    const expectVal = [4, 5, 6];
+    expect(actual).toEqual(expectVal);
+  });
+  it("return array when given 3 args", () => {
+    const actual = range(0, 11, 2);
+    const expectVal = [0, 2, 4, 6, 8, 10];
+    expect(actual).toEqual(expectVal);
+  });
 });
