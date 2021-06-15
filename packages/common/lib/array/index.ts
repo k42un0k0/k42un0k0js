91766acd046f33e014244cf68efac828c589bec1
ShiratoriTenta <tenta1111swan@gmail.com>
2021-06-16 04:14:33 +0900

    fix

--- packages/common/lib/array/index.ts
+++ packages/common/lib/array/index.ts
@@ -3,14 +3,14 @@
  * @param  {number} length
  * @returns number[]
  */
-function range(length: number): number[];
+export function range(length: number): number[];
 /**
  * start～end-1を値を持つ配列を返す
  * @param  {number} start
  * @param  {number} end
  * @returns number[]
  */
-function range(start: number, end: number): number[];
+export function range(start: number, end: number): number[];
 /**
  * start, start + step, start + step * 2, ... start + step * floor((end - start) / step) を値を持つ配列を返す
  * @param  {number} start
@@ -18,8 +18,32 @@ function range(start: number, end: number): number[];
  * @param  {number} step
  * @returns number[]
  */
-function range(start: number, end: number, step: number): number[];
-function range(startOrLength: number, end?: number, step?: number): number[] {
+export function range(start: number, end: number, step: number): number[];
+export function range(
+  startOrLength: number,
+  end?: number,
+  step?: number
+): number[] {
+  function init(
+    startOrLength: number,
+    end: number | undefined,
+    step: number | undefined
+  ) {
+    if (step != null) {
+      return {
+        length: Math.ceil((end! - startOrLength) / step),
+        start: startOrLength,
+      };
+    } else if (end != null) {
+      return {
+        length: end - startOrLength,
+        start: startOrLength,
+      };
+    } else {
+      return { length: startOrLength, start: 0 };
+    }
+  }
+
   let { length, start } = init(startOrLength, end, step);
   function* gen(length: number, cb: (k: number) => number) {
     for (const k of Array(length).keys()) {
@@ -27,24 +51,4 @@ function range(startOrLength: number, end?: number, step?: number): number[] {
     }
   }
   return [...gen(length, (i) => i * (step ?? 1) + start)];
-}
-export { range };
-function init(
-  startOrLength: number,
-  end: number | undefined,
-  step: number | undefined
-) {
-  if (step != null) {
-    return {
-      length: Math.ceil((end! - startOrLength) / step),
-      start: startOrLength,
-    };
-  } else if (end != null) {
-    return {
-      length: end - startOrLength,
-      start: startOrLength,
-    };
-  } else {
-    return { length: startOrLength, start: 0 };
-  }
 }
